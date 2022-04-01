51ad2796006187121100d2864083d6d8adfe55b2
belen-abmg <mgbelen425@gmail.com>
2022-04-01 12:34:47 -0600

    Planet-ix

---
+++ script.js
@@ -0,0 +1,15 @@
+function pasoPag1(){
+  document.getElementById("bienvenida").style.display="none";
+  document.getElementById("pagInfo").style.display="block";
+}
+function infoVideojuego(){
+  document.getElementById("pagInfo").style.display="none";
+  document.getElementById("pagInfoVJ").style.display="block";
+}
+
+function infoEmpresa() {
+  document.getElementById("pagInfoVJ").style.display="none";
+  document.getElementById("pagInfo").style.display="block";
+}
+
+console.log("hola si jalo");
